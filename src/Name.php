e93ff5d61c5fe40435ee55ff55da36144827069c
Menno <menno@mmerlijn.nl>
2022-01-26 09:36:00 +0100

    Name splitter added

--- src/Name.php
+++ src/Name.php
@@ -20,6 +20,7 @@ class Name implements RepositoryInterface
         if ($this->name and (!$this->lastname or !$this->own_lastname)) {
             $this->splitName();
         }
+        $this->splitPrefixesFromNames();
     }
 
     public function toArray(): array
@@ -36,10 +37,42 @@ class Name implements RepositoryInterface
 
     private function splitName()
     {
-        if (strstr($this->name, ",")) {
-            $p = explode(",", $this->name);
-            $this->lastname = trim($p[0]);
+        $parts = explode("-", $this->name);
+        if (count($parts) == 2) {
+            $this->lastname = trim($parts[1]);
+            $this->own_lastname = trim($parts[0]);
+        } else {
+            $this->own_lastname = trim($parts[0]);
         }
-        //TODO
+    }
+
+    private function splitPrefixesFromNames()
+    {
+        $tmp = $this->nameSplitter($this->lastname, $this->prefix);
+        $this->lastname = $tmp['lastname'];
+        $this->prefix = $tmp['prefix'];
+        $tmp = $this->nameSplitter($this->own_lastname, $this->own_prefix);
+        $this->own_lastname = $tmp['lastname'];
+        $this->own_prefix = $tmp['prefix'];
+    }
+
+    //split prefix from name
+    private function nameSplitter($lastname, $prefix = ""): array
+    {
+        $prefixes = ['aan', 'af', 'bij', 'de', 'den', 'der', 'd\'', 'het', '\'t', 'in', 'onder', 'op', 'over', '\'s', 'te', 'ten', 'ter', 'tot', 'uit', 'uijt', 'van', 'ver', 'voor',
+            'a', 'al', 'am', 'auf', 'aus', 'ben', 'bin', 'da', 'dal', 'dalla', 'della', 'das', 'die', 'den', 'der', 'des', 'deca', 'degli', 'dei', 'del', 'di', 'do', 'don', 'dos', 'du', 'el',
+            'i', 'im', 'l', 'la', 'las', 'le', 'les', 'lo', 'los', 'o\'', 'tho', 'thoe', 'thor', 'toe', 'unter', 'vom', 'von', 'vor', 'zu', 'zum', 'zur'];
+        $parts = explode(" ", preg_replace('/,/', " ", $lastname));
+        $lastname = "";
+        foreach ($parts as $part) {
+            if (trim($part)) {
+                if (in_array(strtolower($part), $prefixes)) { //is prefix
+                    $prefix .= " " . strtolower($part);
+                } else { //belongs to lastname
+                    $lastname .= " " . $part;
+                }
+            }
+        }
+        return ['lastname' => trim($lastname), 'prefix' => trim(implode(" ", array_unique(explode(" ", $prefix))))];
     }
 }
--- tests/Unit/NameTest.php
+++ tests/Unit/NameTest.php
@@ -13,4 +13,33 @@ class NameTest extends TestCase
         $this->assertSame($name->own_lastname, "Doe");
         $this->assertArrayHasKey('lastname', $name->toArray());
     }
+
+    public function test_splitter()
+    {
+        $name = new Name(name: "Doe van der");
+        $this->assertSame("van der", $name->own_prefix);
+        $this->assertSame("Doe", $name->own_lastname);
+
+        $name = new Name(name: "Doe, van der");
+        $this->assertSame("van der", $name->own_prefix);
+        $this->assertSame("Doe", $name->own_lastname);
+
+        $name = new Name(name: "van der Vaart - de Groen");
+        $this->assertSame("van der", $name->own_prefix);
+        $this->assertSame("Vaart", $name->own_lastname);
+        $this->assertSame("de", $name->prefix);
+        $this->assertSame("Groen", $name->lastname);
+
+        $name = new Name(name: "van der Vaart-de Groen");
+        $this->assertSame("van der", $name->own_prefix);
+        $this->assertSame("Vaart", $name->own_lastname);
+        $this->assertSame("de", $name->prefix);
+        $this->assertSame("Groen", $name->lastname);
+
+        $name = new Name(name: "Vaart van der-de Groen");
+        $this->assertSame("van der", $name->own_prefix);
+        $this->assertSame("Vaart", $name->own_lastname);
+        $this->assertSame("de", $name->prefix);
+        $this->assertSame("Groen", $name->lastname);
+    }
 }
