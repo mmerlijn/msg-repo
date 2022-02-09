7b82fae1d0f21d70307eac788b6ac6588b3b3412
Menno <menno@mmerlijn.nl>
2022-02-09 10:55:35 +0100

    add orderflagEnum setter

--- src/Enums/ResultFlagEnum.php
+++ src/Enums/ResultFlagEnum.php
@@ -7,4 +7,16 @@ enum ResultFlagEnum: string
     case HIGH = "H"; //new
     case LOW = "L"; //Cancel
     case EMPTY = "";
+
+
+    public static function set(string $flag): self
+    {
+        return match (strtoupper($flag)) {
+            "H" => self::HIGH,
+            "HIGH" => self::HIGH,
+            "LOW" => self::LOW,
+            "L" => self::LOW,
+            default => self::EMPTY,
+        };
+    }
 }
